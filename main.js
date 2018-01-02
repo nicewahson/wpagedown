09e3edfa029f08597c04d43609090cefc21ad822
nicewahson <nicelj92@gmail.com>
2018-01-02 10:18:54 +0800

    r

--- main.js
+++ main.js
@@ -11,26 +11,26 @@ exec(cmdStr, function(err, stdout, stderr) {
         console.log(stdout);
     }
 });
-http.createServer(function(req, res) {
-    console.log(req.url)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-    res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.setHeader("X-Powered-By", ' 3.2.1');
-    if (!req.url.indexOf('favicon') > -1) {
-        // const fc = fs.statSync('./page')
-        fs.readdir('./page', function(err, files) {
+// http.createServer(function(req, res) {
+//     console.log(req.url)
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     res.setHeader("X-Powered-By", ' 3.2.1');
+//     if (!req.url.indexOf('favicon') > -1) {
+//         // const fc = fs.statSync('./page')
+//         fs.readdir('./page', function(err, files) {
 
-            if (!files || !files[0]) {
-                res.setHeader('Content-Type', 'application/json;charset=utf-8')
-                res.statusCode = 500
-                res.end(JSON.stringify({ errCode: 1, msg: '没有可供下载文件' }), 'utf-8')
-            } else {
-                res.setHeader('Content-Type', 'application/octet-stream')
-                res.setHeader('Content-Disposition', 'attachment;filename=' + files[0])
-                fs.createReadStream('./page/' + files[0]).pipe(res)
-            }
-        })
-    }
+//             if (!files || !files[0]) {
+//                 res.setHeader('Content-Type', 'application/json;charset=utf-8')
+//                 res.statusCode = 500
+//                 res.end(JSON.stringify({ errCode: 1, msg: '没有可供下载文件' }), 'utf-8')
+//             } else {
+//                 res.setHeader('Content-Type', 'application/octet-stream')
+//                 res.setHeader('Content-Disposition', 'attachment;filename=' + files[0])
+//                 fs.createReadStream('./page/' + files[0]).pipe(res)
+//             }
+//         })
+//     }
 
-}).listen('8048')
+// }).listen('8048')
