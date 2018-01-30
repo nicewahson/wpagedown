e2066d8e25e6af6dc27a7752012839e46815d22b
nicewahson <nicelj92@gmail.com>
2018-01-30 13:48:31 +0800

    options

--- wxspider.js
+++ wxspider.js
@@ -13,142 +13,145 @@ var gm = require('gm').subClass({
 })
 
 http.createServer(function (req, resp) {
-    if(req.methd=='OPTIONS'){
-        console.log('options')
-    }
-
-    resp.setHeader("Access-Control-Allow-Credentials","true");
+    console.log(req.method)
+    resp.setHeader("Access-Control-Allow-Credentials", "true");
     resp.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-    resp.setHeader("Access-Control-Allow-Headers", "X-Requested-With,token");
+    resp.setHeader("Access-Control-Allow-Headers", "X-Requested-With,token, tookie");
     resp.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     resp.setHeader("X-Powered-By", ' 3.2.1');
-    resp.setHeader("test", ' 3.2.1');
-    req.setEncoding('utf8');
-    req.on('data', function (data) {
-        // console.log(JSON.parse(data.toString()).url, 'data')
-        console.log('cookie is',req.headers)
+    if (req.method == 'OPTIONS') {
+        resp.end()
+    } else {
 
-        if (req.url.indexOf('favicon') == -1) {
-            var cookie = req.headers.cookie;
-            // console.log(req.url.indexOf('download'))
-            let us = JSON.parse(data.toString()).url
-            exec(cmdStr + encodeURIComponent(us), function (err, stdout, stderr) {
-                fs.readdir('./page', function (err, outfiles) {
+        resp.setHeader("test", ' 3.2.1');
+        req.setEncoding('utf8');
+        req.on('data', function (data) {
+            // console.log(JSON.parse(data.toString()).url, 'data')
+            console.log('cookie is', req.headers)
 
-                    if (err) {
-                        console.log('open error:' + stderr);
-                    } else {
-                        console.log('start >>>>>>>>>>')
-                        if (req.url.indexOf('download') > -1) { //下载
-                            console.log('begin download >>>>>>>>>>')
-                            var stm = fs.createReadStream('./page/' + outfiles[0]).pipe(resp)
-                            stm.on('finish', function () {
-                                console.log('finish success')
-                                fs.unlinkSync('./page/' + outfiles[0])
-                            })
-                        } else if (req.url.indexOf('generate') > -1) { //一键生成
-                            console.log('begin generate >>>>>>>>>>')
-                            fs.readdir('./page', function (err, files) {
-                                if (err) {
-                                    console.log('open error:' + stderr);
-                                } else {
-                                    getPixels('./page/' + files[0], function (err, pixels) {
-                                        console.log('height and width',pixels.shape.slice())
-                                        var psHeight = pixels.shape.slice()[1],
-                                            urls = [],
-                                            delta = 0,
-                                            ms = (psHeight - Math.floor(psHeight / constHeight) * constHeight) > 0 ? Math.floor(psHeight / constHeight) + 1 : Math.floor(psHeight / constHeight)
-                                        var crops = function (d) {
-                                            gm('./page/' + files[0]).crop(750, constHeight, 0, 240 + delta * constHeight).autoOrient().write('./temp/t' + d + '.jpg', function (err) {
+            if (req.url.indexOf('favicon') == -1) {
+                var cookie = req.headers.cookie;
+                // console.log(req.url.indexOf('download'))
+                let us = JSON.parse(data.toString()).url
+                exec(cmdStr + encodeURIComponent(us), function (err, stdout, stderr) {
+                    fs.readdir('./page', function (err, outfiles) {
 
-                                                delta += 1
+                        if (err) {
+                            console.log('open error:' + stderr);
+                        } else {
+                            console.log('start >>>>>>>>>>')
+                            if (req.url.indexOf('download') > -1) { //下载
+                                console.log('begin download >>>>>>>>>>')
+                                var stm = fs.createReadStream('./page/' + outfiles[0]).pipe(resp)
+                                stm.on('finish', function () {
+                                    console.log('finish success')
+                                    fs.unlinkSync('./page/' + outfiles[0])
+                                })
+                            } else if (req.url.indexOf('generate') > -1) { //一键生成
+                                console.log('begin generate >>>>>>>>>>')
+                                fs.readdir('./page', function (err, files) {
+                                    if (err) {
+                                        console.log('open error:' + stderr);
+                                    } else {
+                                        getPixels('./page/' + files[0], function (err, pixels) {
+                                            console.log('height and width', pixels.shape.slice())
+                                            var psHeight = pixels.shape.slice()[1],
+                                                urls = [],
+                                                delta = 0,
+                                                ms = (psHeight - Math.floor(psHeight / constHeight) * constHeight) > 0 ? Math.floor(psHeight / constHeight) + 1 : Math.floor(psHeight / constHeight)
+                                            var crops = function (d) {
+                                                gm('./page/' + files[0]).crop(750, constHeight, 0, 240 + delta * constHeight).autoOrient().write('./temp/t' + d + '.jpg', function (err) {
 
-                                                if (!err) {
+                                                    delta += 1
 
-                                                    if (delta < ms) {
-                                                        crops(delta)
-                                                    } else {
-                                                        // console.log('end up')
-                                                        fs.readdir('./temp', function (err, files) {
-                                                            var pos = [];
-                                                            for (var i = 0; i < files.length; i++) {
-                                                                pos.push(upPic(files[i]))
-                                                            }
+                                                    if (!err) {
 
-                                                            Promise.all(pos).then(function (res) {
-                                                                // console.log('result urls', res)
-                                                                urls = res;
-                                                                upload()
-                                                            })
-                                                        })
+                                                        if (delta < ms) {
+                                                            crops(delta)
+                                                        } else {
+                                                            // console.log('end up')
+                                                            fs.readdir('./temp', function (err, files) {
+                                                                var pos = [];
+                                                                for (var i = 0; i < files.length; i++) {
+                                                                    pos.push(upPic(files[i]))
+                                                                }
 
-                                                        function upPic(d) {
-                                                            var formData = {
-                                                                pic: fs.createReadStream('./temp/' + d)
-                                                            }
-                                                            return new Promise(function (resolve, reject) {
-                                                                request.post({
-                                                                    url: 'http://cardmanage-server.dev.sanqimei.com/upload/addTempImage',
-                                                                    formData: formData
-                                                                }, function (err, res, body) {
-                                                                    console.log('url',body)
-                                                                    if (!err) {
-                                                                        resolve(body)
-                                                                    } else {
-                                                                        reject('上传失败')
-                                                                    }
+                                                                Promise.all(pos).then(function (res) {
+                                                                    // console.log('result urls', res)
+                                                                    urls = res;
+                                                                    upload()
                                                                 })
                                                             })
+
+                                                            function upPic(d) {
+                                                                var formData = {
+                                                                    pic: fs.createReadStream('./temp/' + d)
+                                                                }
+                                                                return new Promise(function (resolve, reject) {
+                                                                    request.post({
+                                                                        url: 'http://cardmanage-server.dev.sanqimei.com/upload/addTempImage',
+                                                                        formData: formData
+                                                                    }, function (err, res, body) {
+                                                                        console.log('url', body)
+                                                                        if (!err) {
+                                                                            resolve(body)
+                                                                        } else {
+                                                                            reject('上传失败')
+                                                                        }
+                                                                    })
+                                                                })
+                                                            }
                                                         }
                                                     }
-                                                }
-                                            });
-                                        }
+                                                });
+                                            }
 
-                                        crops(delta)
+                                            crops(delta)
 
-                                        function upload() {
-                                            console.log('urls', urls)
-                                            console.log('begin upload')
-                                            var last = urls.map(function (item) {
-                                                return {
-                                                    pic: item
-                                                }
-                                            })
-                                            // console.log(last)
-                                            request.post({
-                                                url: 'http://cardmanage-server.dev.sanqimei.com/advertisementPage/addAdvertisementPage',
-                                                headers: {
-                                                    'Cookie': cookie
-                                                }
-                                            }, function (err, res, body) {
-                                                console.log('end upload')
-                                                console.log(err, body)
-                                                fs.readdir('./temp', function (err, files) {
-                                                    files.forEach(function (item) {
-                                                        fs.unlink('./temp/'+item)
-                                                    })
+                                            function upload() {
+                                                console.log('urls', urls)
+                                                console.log('begin upload')
+                                                var last = urls.map(function (item) {
+                                                    return {
+                                                        pic: item
+                                                    }
                                                 })
+                                                // console.log(last)
+                                                request.post({
+                                                    url: 'http://cardmanage-server.dev.sanqimei.com/advertisementPage/addAdvertisementPage',
+                                                    headers: {
+                                                        'Cookie': cookie
+                                                    }
+                                                }, function (err, res, body) {
+                                                    console.log('end upload')
+                                                    console.log(err, body)
+                                                    fs.readdir('./temp', function (err, files) {
+                                                        files.forEach(function (item) {
+                                                            fs.unlink('./temp/' + item)
+                                                        })
+                                                    })
 
-                                                fs.unlinkSync('./page/' + outfiles[0])
-                                                resp.setHeader('Content-Type', 'application/json;charset=utf-8')
-                                                resp.end(body, 'utf8')
+                                                    fs.unlinkSync('./page/' + outfiles[0])
+                                                    resp.setHeader('Content-Type', 'application/json;charset=utf-8')
+                                                    resp.end(body, 'utf8')
 
-                                            }).form({
-                                                title: 'title'+(+new Date),
-                                                picList: JSON.stringify(last)
-                                            })
-                                        }
-                                    })
-                                    console.log('end generate >>>>>>>');
-                                }
-                            });
+                                                }).form({
+                                                    title: 'title' + (+new Date),
+                                                    picList: JSON.stringify(last)
+                                                })
+                                            }
+                                        })
+                                        console.log('end generate >>>>>>>');
+                                    }
+                                });
 
+                            }
                         }
-                    }
-                });
-            })
-        }
-    })
+                    });
+                })
+            }
+        })
+
+    }
 
 }).listen('8048')
